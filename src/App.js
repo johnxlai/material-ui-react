762d2a28e55a5f389e9c0149da0b13add9f5dc48
John Lai <lai.john.py@gmail.com>
2024-06-24 13:00:43 -0400

    Add grid item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import {
   Typography,
+  Button,
   AppBar,
   Card,
   CardActions,
@@ -13,8 +14,11 @@ import {
 } from '@mui/material';
 
 import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 function App() {
+  const color = 'white';
+
   return (
     <>
       <CssBaseline />
@@ -24,6 +28,44 @@ function App() {
           <Typography variant="h6">Photo Album</Typography>
         </Toolbar>
       </AppBar>
+
+      <main>
+        <div>
+          <Container maxWidth="sm" style={{ marginTop: '100px' }}>
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom>
+              Payment Gateway
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+              fugiat nemo veritatis labore, quaerat cupiditate dolore doloremque
+              corporis fugit aliquam blanditiis esse nam architecto deleniti
+              laboriosam officia tempora! Deleniti, error.
+            </Typography>
+            <div>
+              <Grid container spacing={2}>
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Pay Now
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Pay Later
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+      </main>
     </>
   );
 }
